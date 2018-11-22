40db75c4ea52ab577693a699b81b7e243587332a
lopree <zhutong1993@gmail.com>
2018-11-22 11:10:07 +0800

    del

--- HelloPy/Practice.py
+++
@@ -1,7 +0,0 @@
-import re
-stringNmae = "【整租】泊寓 文一路 万科泊寓文一"
-print(stringNmae.split()[0])
-print(type(stringNmae.split()[0]))
-strFir = re.split('【|】',stringNmae.split()[0])[1]
-print(strFir)
-# print(type(strFir[0]))
