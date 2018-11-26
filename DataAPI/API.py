119de9b178258d2cb83d8ba2cd88c0c3e0288a22
lopree <zhutong1993@gmail.com>
2018-11-26 14:11:30 +0800

    api

--- DataAPI/API.py
+++ DataAPI/API.py
@@ -3,9 +3,9 @@ from flask_restful import Api,Resource
 
 app = Flask(__name__)
 
-@app.route('/HELLOWORLD')
-def hello_world():
-    return 'Hello World!'
+@app.route('/')
+def index():
+    return "Hello, World!"
 
 if __name__ == '__main__':
     app.run(debug=True)
